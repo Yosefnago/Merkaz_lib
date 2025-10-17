2a7e278a78dc63ff22465f3befc6ab4b66a475f0
Yosefnago <yosinago5@gmail.com>
2025-10-17 15:53:06 +0300

    feat(register): add registration component and API contract doc

---
+++ merkaz-frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes} from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgotpass/forgotpass.component';
+import {DashboardComponent} from './components/dashborad/dashboard.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'dashboard' ,component: DashboardComponent}
+];
+
